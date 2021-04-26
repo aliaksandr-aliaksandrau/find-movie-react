246c4b9dd5d49131d21c8027ae919d0ec0beb3bc
Aliaksandr Aliaksandrau <aliaksandr_aliaksandrau1@epam.com>
2021-04-26 09:39:47 +0300

    feat: page not found layout

--- src/containers/PageNotFound/PageNotFound.tsx
+++ src/containers/PageNotFound/PageNotFound.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import Footer from "../../components/Footer/Footer";
+import Logo from "../../components/Logo/Logo";
+
+import "./PageNotFound.scss";
 
 export default function PageNotFound() {
   return (
-    <>
-      <div>404 error</div>
+    <div className="PageNotFound__container">
+      <Logo />
+      <div className="PageNotFound__error">404 error</div>
       <Footer />
-    </>
+    </div>
   );
 }
